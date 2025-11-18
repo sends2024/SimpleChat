37c3e17f53252e15f0e55eed280bbca457c3f5c2
Plzlight2 <1916329707@qq.com>
2025-11-18 23:41:15 +0800

    182341

---
+++ server/router/router.go
@@ -0,0 +1,50 @@
+package router
+
+import (
+	"server/api"
+	"server/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Static("/uploads", "./uploads")
+
+	r.Use(middlewares.CORS())
+
+	origen := r.Group("/api")
+
+	origen.POST("/users/register", api.Register)
+	origen.POST("/users/login", api.Login)
+
+	user := origen.Group("/user")
+	user.Use(middlewares.Auth())
+	{
+		user.PATCH("/password", api.ChangePassword)
+		user.PUT("/avatar", api.ChangeAvatar)
+	}
+
+	channels := origen.Group("/channels")
+	channels.Use(middlewares.Auth())
+	{
+		channels.POST("/create", api.CreateChannel)
+		channels.POST("/join", api.JoinChannel)
+		channels.POST("/:channel_id/leave", api.LeaveChannel)
+		channels.GET("/list", api.GetChannels)
+		channels.GET("/:channel_id/history", api.GetHistory)
+	}
+
+	channel := origen.Group("/channel")
+	channel.Use(middlewares.Auth())
+	{
+		channel.DELETE("/:channel_id", api.DeleteChannel)
+		channel.DELETE("/:channel_id/member/:user_id", api.RemoveMember)
+		channel.PATCH("/:channel_id", api.ChangeChannelName)
+		channel.GET("/:channel_id/members", api.GetMembers)
+		channel.GET("/invite", api.GetInviteCode)
+
+	}
+
+	return r
+}
