1c71b1944ab15823916c5114d88bcba0be4648e5
Plzlight2 <1916329707@qq.com>
2025-11-18 19:32:03 +0800

    181931

--- server/src/main/java/com/simplechat/repository/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simplechat.repository;
-
-import com.simplechat.entity.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelRepository extends JpaRepository<Channel, String> {
-}
